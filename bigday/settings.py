8d1a40998889961d91da7e35b109e93fd04e72fe
Mitraj Parmar <mrpmrp@amazon.com>
2024-03-22 15:09:35 -0700

    just fix the env

--- bigday/settings.py
+++ bigday/settings.py
@@ -54,11 +54,11 @@ if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
+    AWS_STORAGE_BUCKET_NAME = "test"]
+    AWS_S3_REGION_NAME = "test"]
 
-    AWS_S3_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_S3_ACCESS_KEY_ID = "test"]
+    AWS_S3_SECRET_ACCESS_KEY = "test"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -100,11 +100,11 @@ if 'CODEBUILD_CI' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': "test",
+            'USER': "test",
+            'PASSWORD': "test",
+            'HOST': "test",
+            'PORT': "test",
         }
     }
 else:
